0b6564db1a539f612b1702162a99128bd33873d7
ben <phattrien0222@gmail.com>
2021-10-11 23:37:40 +0700

    curd department

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const mainRoutes = require('./src/route/main');
-const userRoutes = require('./src/route/user_route');
-const status = require('./src/utils/status');
-const dotenv = require('dotenv');
-const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const mainRoutes = require("./src/route/main");
+const userRoutes = require("./src/route/user_route");
+const departmentRoutes = require("./src/route/department_route");
+const status = require("./src/utils/status");
+const dotenv = require("dotenv");
+const express = require("express");
 
 // get config vars
 dotenv.config();
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
-
+app.set("port", process.env.PORT || 3000);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger("dev"));
 
 var db = process.env.URI_DB_DEV || process.env.URI_DB_PRODUCT;
-mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-        console.log('Database connected')
-    })
-    .catch((error) => {
-        console.log(error);
-        console.log('Error connecting to database')
-    });
+mongoose
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("Database connected");
+	})
+	.catch((error) => {
+		console.log(error);
+		console.log("Error connecting to database");
+	});
 
-app.get('/', function (req, res) {
-    if (!res.headersSent) {
-        res.status(status.success).json({
-            message: 'TECH NFD API RESTFUL',
-        });
-    }
-    res.end();
+app.get("/", function (req, res) {
+	if (!res.headersSent) {
+		res.status(status.success).json({
+			message: "TECH NFD API RESTFUL",
+		});
+	}
+	res.end();
 });
 
-app.use('/api/', mainRoutes);
-app.use('/api/user', userRoutes);
+app.use("/api/", mainRoutes, departmentRoutes);
+app.use("/api/user", userRoutes);
 
-
-app.listen(app.get('port'), function () {
-    console.log('Listening on port ' + app.get('port'));
-    console.log(db);
+app.listen(app.get("port"), function () {
+	console.log("Listening on port " + app.get("port"));
+	console.log(db);
 });
