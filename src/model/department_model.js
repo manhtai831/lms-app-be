8f31240be87c2e5c64b641377965154a01ffebc5
ben <phattrien0222@gmail.com>
2021-10-12 10:45:49 +0700

    clear form

--- src/model/department_model.js
+++ src/model/department_model.js
@@ -4,16 +4,27 @@ const autoIncrement = require("mongoose-auto-increment");
 mongoose.Promise = global.Promise;
 
 const departmentModel = new mongoose.Schema({
-	id: Number,
-	name: String,
+	id: {
+		type: Number,
+		immutable: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	description: String,
+	createAt: Date,
+	createBy: Object,
+	updateAt: Date,
+	updateBy: Object,
 });
 
 autoIncrement.initialize(mongoose.connection);
 departmentModel.plugin(autoIncrement.plugin, {
-	model: "department", // collection or table name in which you want to apply auto increment
+	model: "DepartmentModel", // collection or table name in which you want to apply auto increment
 	field: "id", // field of model which you want to auto increment
 	startAt: 1, // start your auto increment value from 1
 	incrementBy: 1, // incremented by 1
 });
 
-module.exports = mongoose.model("department", departmentModel);
+module.exports = mongoose.model("DepartmentModel", departmentModel);
--- src/model/user_model.js
+++ src/model/user_model.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose');
-const autoIncrement = require('mongoose-auto-increment');
-const Course = require('../model/tetst_model');
+const mongoose = require("mongoose");
+const autoIncrement = require("mongoose-auto-increment");
 
 mongoose.Promise = global.Promise;
 
-
 const userModel = new mongoose.Schema({
-    id: Number,
-    name: {
-        type: String,
-        required: true,
-    },
-    userName: String,
-    password: String,
-    email: String,
-    phoneNumber: String,
-    birth:String,
-    token: String,
-    permission: [],
-    avatar:Object
+	id: Number,
+	name: {
+		type: String,
+	},
+	userName: {
+		type: String,
+		immutable: true,
+	},
+	password: String,
+	email: String,
+	phoneNumber: String,
+	birth: String,
+	token: String,
+	permission: [],
+	avatar: String,
 });
 
 autoIncrement.initialize(mongoose.connection);
 userModel.plugin(autoIncrement.plugin, {
-    model: "User", // collection or table name in which you want to apply auto increment
-    field: "id", // field of model which you want to auto increment
-    startAt: 1, // start your auto increment value from 1
-    incrementBy: 1, // incremented by 1
+	model: "User", // collection or table name in which you want to apply auto increment
+	field: "id", // field of model which you want to auto increment
+	startAt: 1, // start your auto increment value from 1
+	incrementBy: 1, // incremented by 1
 });
 
-module.exports = mongoose.model('User', userModel);
+module.exports = mongoose.model("User", userModel);
